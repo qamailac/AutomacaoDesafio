f9e2476f83c112cf7faf498089dd499991c8332e
qamailac <77470966+qamailac@users.noreply.github.com>
2021-08-22 23:06:24 -0300

    Delete cypress/support/elements directory

--- cypress/support/elements/LoginElements.js
+++
@@ -1,8 +0,0 @@
-class LoginElements {
-    botaoPaginaLogin = () => { return 'not-logged' }
-  
-    botaoEmailSenha = () => { return 'btn-block btn-large vtexIdUI-others-send-email' }
-
-  }
-  
-  export default LoginElements;
